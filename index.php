22a5ae66eea0cd0090e0382c1cd15886a3bc6b0e
guilherme <guigamergg94@gmail.com>
2024-09-26 12:46:52 -0300

    comit de reinicio  On branch main  Your branch is up to date with 'origin/main'.  Changes to be committed: 	deleted:    avaliacoes.html 	deleted:    cadastro.html 	deleted:    css/index.css 	deleted:    css/login.css 	deleted:    css/sobre.css 	modified:   css/styles.css 	deleted:    img/logo.png 	deleted:    index.html 	new file:   index.php 	deleted:    js/cadastro.js 	deleted:    js/index.js 	deleted:    js/login.js 	deleted:    login.html 	deleted:    sobre.html

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>ShelfShare - Troca de Livros</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="css/styles.css">
+    </head>
+    <body>
+        <!-- Header -->
+        <header class="index-header">
+            
+        </header>
+        
+        <!-- Main -->
+        <div class="index-main">
+            
+        </div>
+    </body>
+</html>
