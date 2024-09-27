37a7acc95b0b4739e5e6d8d57e76fd48830e8dfc
20221tiimi0149 <guilhermesoaresvixbr@gmail.com>
2024-09-27 08:53:21 -0300

    adição de divs pra conserto  Changes to be committed: 	modified:   css/styles.css 	modified:   index.php

--- index.php
+++ index.php
@@ -7,11 +7,17 @@
         <meta name="description" content="">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="stylesheet" href="css/styles.css">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     </head>
     <body>
         <!-- Header -->
         <header class="index-header">
-            
+            <div class="left-header">
+            </div>
+            <div class="right-header">
+                <button class="btn-primary">Cadastre-se</button>
+                <button class="btn-primary">Login</button>
+            </div>
         </header>
         
         <!-- Main -->
